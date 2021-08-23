983e116757340e0e27c41180886928ffcbe86c2d
gustiandra <gustiandra49@student.untan.ac.id>
2021-08-23 12:26:41 +0700

    Tampilan Login

--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [function () {
-    return view('welcome');
-}]);
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
     // Class
     Route::get('/kelas-murid', [StudentClassController::class, 'index'])->name('studentclass.index');
